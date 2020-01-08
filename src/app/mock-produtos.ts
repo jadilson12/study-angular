da512c7dd968c8c99565d0db3f622c8f1227c4ba
Jadilson Guedes <jadilson12@gmail.com>
2020-01-07 22:54:18 -0300

    :add: mock-produto

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,28 @@
+import { Produtos } from './produto/produto-class.component';
+
+export const PRODUTOS: Produtos[] = [
+  {
+    id: 11,
+    nome: 'Server 1',
+    descricao: '',
+    categoria: '',
+  },
+  {
+    id: 11,
+    nome: 'Server 1',
+    descricao: '',
+    categoria: '',
+  },
+  {
+    id: 11,
+    nome: 'Server 1',
+    descricao: '',
+    categoria: '',
+  },
+  {
+    id: 11,
+    nome: 'Server 1',
+    descricao: '',
+    categoria: '',
+  },
+];
