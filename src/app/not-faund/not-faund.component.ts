b93cd6306a600a26c5fa55fdce9a8e8ff0bc65c3
Jadilson Guedes <jadilson12@gmail.com>
2020-01-07 22:48:44 -0300

    :add: module not-found

---
+++ src/app/not-faund/not-faund.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-faund',
+  templateUrl: './not-faund.component.html',
+  styleUrls: ['./not-faund.component.scss']
+})
+export class NotFaundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/not-faund/not-faund.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFaundComponent } from './not-faund.component';
+
+@NgModule({
+  declarations: [NotFaundComponent],
+  imports: [CommonModule],
+})
+export class NotFaundModule {}
