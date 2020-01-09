4cae1ba93dd7fcbdb2578e3844a52b07aa792ae6
Jadilson Guedes <jadilson12@gmail.com>
2020-01-08 22:13:01 -0300

    :add: tabela de produtos

--- src/app/produto/produto-list/produto-list.component.ts
+++ src/app/produto/produto-list/produto-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { PRODUTOS } from 'src/app/mock-produtos';
+import { Produtos } from '../produto-class.component';
+import { Categorias } from 'src/app/categoria/categoria-class.component';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-produto-list',
   templateUrl: './produto-list.component.html',
-  styleUrls: ['./produto-list.component.scss']
+  styleUrls: ['./produto-list.component.scss'],
 })
 export class ProdutoListComponent implements OnInit {
+  public data: Produtos[];
+  public _categorias: Categorias[];
+  public displayedColumns = ['id', 'nome', 'descricao', 'categoria'];
+  public dataSource = new MatTableDataSource<Produtos>(PRODUTOS);
 
-  constructor() { }
+  constructor(private _produtoService: ProdutoService) {}
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.getProdutos();
   }
 
+  public getProdutos() {
+    this._produtoService.getProdutos().subscribe(itens => (this.data = itens));
+  }
 }
