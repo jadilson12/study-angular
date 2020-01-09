0ac3be000d3c082e9c7fc42de93ccd2ee42459c2
Jadilson Guedes <jadilson12@gmail.com>
2020-01-08 22:13:46 -0300

    :update: app principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,17 @@ import { CategoriaComponent } from './categoria/categoria.component';
 import { HomeComponent } from './home/home.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { NotFaundComponent } from './not-faund/not-faund.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+import { ProdutoFormComponent } from './produto/produto-form/produto-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'produto', component: ProdutoComponent },
-  { path: 'categoria', component: CategoriaComponent },
+  { path: 'produtos', component: ProdutoComponent },
+  { path: 'produto/novo', component: ProdutoFormComponent },
+  { path: 'categorias', component: CategoriaComponent },
+  { path: 'categoria/nova', component: CategoriaFormComponent },
+  { path: 'categoria/:id', component: CategoriaFormComponent },
   { path: '**', component: NotFaundComponent, pathMatch: 'full' },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'study';
+  title = 'Loja';
 }
