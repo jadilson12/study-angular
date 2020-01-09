984c14d457d689540c17c0bf60376d31f110faa6
Jadilson Guedes <jadilson12@gmail.com>
2020-01-08 22:12:07 -0300

    :add: listagem das categorias

--- src/app/categoria/categoria-list/categoria-list.component.ts
+++ src/app/categoria/categoria-list/categoria-list.component.ts
@@ -1,88 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { merge, Observable, of as observableOf } from 'rxjs';
-import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+
+import { Categorias } from './../categoria-class.component';
+import { CategoriaService } from '../categoria.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { CATEGORIA } from 'src/app/mock-categorias';
 
 @Component({
   selector: 'app-categoria-list',
   templateUrl: './categoria-list.component.html',
-  styleUrls: ['./categoria-list.component.scss']
+  styleUrls: ['./categoria-list.component.scss'],
 })
-export class CategoriaListComponent implements AfterViewInit  {
-  // categorias = [
-  //   { nome: 'nome', descricao: '121', tipo: 'dfdf', setor: 'TI', date: '12', isPublicado: true },
-  //   { nome: 'nome', descricao: '121', tipo: 'dfdf', setor: 'TI', date: '12', isPublicado: true },
-  //   { nome: 'nome', descricao: '121', tipo: 'dfdf', setor: 'TI', date: '12', isPublicado: true },
-  //   { nome: 'nome', descricao: '121', tipo: 'dfdf', setor: 'TI', date: '12', isPublicado: true }
-  // ]
-  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
-  exampleDatabase: ExampleHttpDatabase | null;
-  data: GithubIssue[] = [];
+export class CategoriaListComponent implements OnInit {
+  public data: Categorias[];
+  public displayedColumns = ['id', 'nome', 'descricao', 'acoes'];
+  public dataSource = new MatTableDataSource<Categorias>(CATEGORIA);
 
-  resultsLength = 0;
-  isLoadingResults = true;
-  isRateLimitReached = false;
+  constructor(private categoriaService: CategoriaService) {}
 
-  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort: MatSort;
-
-  constructor(private _httpClient: HttpClient) { }
-
-  ngAfterViewInit() {
-    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
-
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.exampleDatabase!.getRepoIssues(
-            this.sort.active, this.sort.direction, this.paginator.pageIndex);
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = false;
-          this.resultsLength = data.total_count;
-
-          return data.items;
-        }),
-        catchError(() => {
-          this.isLoadingResults = false;
-          // Catch if the GitHub API has reached its rate limit. Return empty data.
-          this.isRateLimitReached = true;
-          return observableOf([]);
-        })
-      ).subscribe(data => this.data = data);
+  public ngOnInit() {
+    this.getCategoria();
   }
-}
 
-export interface GithubApi {
-  items: GithubIssue[];
-  total_count: number;
-}
+  public getCategoria() {
+    this.categoriaService
+      .getCategorias()
+      .subscribe(itens => (this.data = itens));
+  }
+  public edit(id: number): void {
+    this.categoriaService.edit(id);
+  }
 
-export interface GithubIssue {
-  created_at: string;
-  number: string;
-  state: string;
-  title: string;
-}
-
-/** An example database that the data source uses to retrieve data for the table. */
-export class ExampleHttpDatabase {
-  constructor(private _httpClient: HttpClient) { }
-
-  getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
-    const href = 'https://api.github.com/search/issues';
-    const requestUrl =
-      `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page + 1}`;
-
-    return this._httpClient.get<GithubApi>(requestUrl);
+  public delete(id: number): void {
+    this.categoriaService.delete(id);
   }
 }
