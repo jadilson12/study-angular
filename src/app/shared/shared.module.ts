723ae872255d461a689a1395a80f4e94034af287
Jadilson Guedes <jadilson12@gmail.com>
2020-01-12 14:02:40 -0300

    :add: Implementa footer form component para os componente

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DialogService } from './dialog.service';
-import { AlertService } from './alert.service';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MessageErrorComponent } from './message-error/message-error.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
+import { DialogService } from './dialog.service';
+import { AlertService } from './alert.service';
+import { FooterFormComponent } from './footer-form/footer-form.component';
 @NgModule({
-  declarations: [MessageErrorComponent],
+  declarations: [MessageErrorComponent, FooterFormComponent],
   imports: [
     CommonModule,
+
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
     MatSnackBarModule,
     MatIconModule,
+    MatGridListModule,
+    MatTooltipModule,
   ],
-  exports: [MessageErrorComponent],
+  exports: [MessageErrorComponent, FooterFormComponent],
   providers: [DialogService, AlertService],
 })
 export class SharedModule {}
